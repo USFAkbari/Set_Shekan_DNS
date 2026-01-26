341d0fabfa6ea6cbffb7837d4fa826dcf8040043
usfakbari <akbari7694@gmail.com>
2026-01-26 20:27:59 +0330

    +Add Pro Section

--- shekan_dns.sh
+++ shekan_dns.sh
@@ -3,8 +3,41 @@
 SYSTEMD_CONF="/etc/systemd/resolved.conf"
 BACKUP_CONF="/etc/systemd/resolved.conf.backup"
 
-add_shekan_dns() {
-  echo "[+] Adding Shekan free DNS..."
+add_shekan_pro() {
+  echo "[+] Adding Shekan free DNS=178.22.122.101 185.51.200.1"
+
+  # Backup original config if not already backed up
+  if [ ! -f "$BACKUP_CONF" ]; then
+    cp "$SYSTEMD_CONF" "$BACKUP_CONF"
+    echo "[+] Backup created at $BACKUP_CONF"
+  else
+    echo "[i] Backup already exists: $BACKUP_CONF"
+  fi
+
+  # Update DNS to Shekan
+  sed -i 's/^#DNS=.*/DNS=178.22.122.101 185.51.200.1/' $SYSTEMD_CONF ||
+  echo "DNS=178.22.122.101 185.51.200.1" >>$SYSTEMD_CONF
+
+  # Add DNS Fallback
+  sed -i 's/^#FallbackDNS=.*/FallbackDNS=8.8.8.8/' $SYSTEMD_CONF
+  sed -i 's/^FallbackDNS=.*/FallbackDNS=8.8.8.8/' $SYSTEMD_CONF
+
+  # Ensure DNSStubListener=no
+  sed -i 's/^#DNSStubListener=.*/DNSStubListener=no/' $SYSTEMD_CONF
+  sed -i 's/^DNSStubListener=.*/DNSStubListener=no/' $SYSTEMD_CONF
+
+  # Link resolv.conf to systemd-resolved
+  ln -sf /etc/systemd/resolved.conf /etc/resolved.conf
+
+  systemctl restart systemd-resolved
+  echo "--------------------------------------------------------------"
+  cat /etc/resolved.conf
+  echo "--------------------------------------------------------------"
+  echo "[+] Shekan DNS applied successfully!"
+}
+
+add_shekan_Free() {
+  echo "[+] Adding Shekan free DNS=178.22.122.100 185.51.200.2"
 
   # Backup original config if not already backed up
   if [ ! -f "$BACKUP_CONF" ]; then
@@ -16,17 +49,23 @@ add_shekan_dns() {
 
   # Update DNS to Shekan
   sed -i 's/^#DNS=.*/DNS=178.22.122.100 185.51.200.2/' $SYSTEMD_CONF ||
-    echo "DNS=178.22.122.100 185.51.200.2" >>$SYSTEMD_CONF
+  echo "DNS=178.22.122.100 185.51.200.2" >>$SYSTEMD_CONF
+  
+  # Add DNS Fallback
+  sed -i 's/^#FallbackDNS=.*/FallbackDNS=8.8.8.8/' $SYSTEMD_CONF
+  sed -i 's/^FallbackDNS=.*/FallbackDNS=8.8.8.8/' $SYSTEMD_CONF
 
   # Ensure DNSStubListener=no
   sed -i 's/^#DNSStubListener=.*/DNSStubListener=no/' $SYSTEMD_CONF
   sed -i 's/^DNSStubListener=.*/DNSStubListener=no/' $SYSTEMD_CONF
 
   # Link resolv.conf to systemd-resolved
-  ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
+  ln -sf /etc/systemd/resolved.conf /etc/resolved.conf
 
   systemctl restart systemd-resolved
-
+  echo "--------------------------------------------------------------"
+  cat /etc/resolved.conf
+  echo "--------------------------------------------------------------"
   echo "[+] Shekan DNS applied successfully!"
 }
 
@@ -75,14 +114,16 @@ clear
 echo "======================================="
 echo "     Shekan DNS Manager (systemd)       "
 echo "======================================="
-echo "1) Add Shekan free DNS"
-echo "2) Roll back DNS settings"
-echo -n "Choose an option (1/2): "
+echo "1) Add Pro Shekan"
+echo "2) Add Free Shekan"
+echo "3) Roll back DNS settings"
+echo -n "Choose an option (1~3): "
 read choice
 
 case $choice in
-1) add_shekan_dns ;;
-2) rollback_dns ;;
+1) add_shekan_pro ;;
+2) add_shekan_dns ;;
+3) rollback_dns ;;
 *)
   echo "Invalid option"
   exit 1
